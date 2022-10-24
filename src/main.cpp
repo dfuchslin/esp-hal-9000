acd4ba809f3d77d530ad615b32ff89718a8117b1
David Füchslin <david.fuchslin@gmail.com>
2022-10-24 21:51:56 +0200

    limit checks

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,13 @@ void loop()
 {
   float val = (exp(sin(millis() / 2000.0 * PI)) - 0.368) * 108.0;
   analogWrite(1, val);
-  analogWrite(2, 255 - val);
+  if (val > 255.0)
+  {
+    val = 255.0;
+  }
+  if (val < 0.0)
+  {
+    val = 0.0;
+  }
+  analogWrite(2, 255.0 - val);
 }
