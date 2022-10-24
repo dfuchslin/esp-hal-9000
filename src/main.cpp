2264a70dded4d1b77dce702e8747e1b1c679859b
David Füchslin <david.fuchslin@gmail.com>
2022-10-24 21:37:38 +0200

    use more pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
+// cool breathing: https://thingpulse.com/breathing-leds-cracking-the-algorithm-behind-our-breathing-pattern/
+
 #include <Arduino.h>
 #include <math.h>
 
 void setup()
 {
+  pinMode(1, OUTPUT); // onboard led
   pinMode(2, OUTPUT);
 }
 
 void loop()
 {
   float val = (exp(sin(millis() / 2000.0 * PI)) - 0.368) * 108.0;
-  analogWrite(2, val);
+  analogWrite(1, val);
+  analogWrite(2, 255 - val);
 }
